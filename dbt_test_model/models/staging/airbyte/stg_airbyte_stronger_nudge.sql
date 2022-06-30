61a6473e252fb0e7821fbdf4a2ed705db7c72408
schlattk <konrad@pensionbee.com>
2022-06-30 17:57:50 +0100

    correct typos

--- dbt_test_model/models/staging/airbyte/stg_airbyte_stronger_nudge.sql
+++ dbt_test_model/models/staging/airbyte/stg_airbyte_stronger_nudge.sql
@@ -8,7 +8,7 @@ extracted as (
         _airbyte_data['Id']::varchar(255) as id,
         _airbyte_data['Account__c']::varchar(255) as account__c,
         _airbyte_data['Lead__c']::varchar(255) as lead__c,
-        _airbyte_data['CreatedDate']::date as created_date,
+        left(_airbyte_data['CreatedDate'], 19)::timestamp as created_date,
         _airbyte_data['Guidance_Appointment_Choice_Timestamp__c']::date as guidance_appointment_date,
         _airbyte_data['Guidance_Appointment_Choice__c']::varchar(255) as guidance_appointment_choice,
         _airbyte_data['Stronger_Nudge_Status__c']::varchar(50) as stronger_nudge_status,
@@ -26,7 +26,7 @@ ranged as (
         guidance_appointment_date,
         guidance_appointment_choice,
         stronger_nudge_status,
-        surfacing_event
+        surfacing_event,
         max(_airbyte_emitted_at) as _airbyte_last_emitted
     from
         extracted
@@ -40,6 +40,7 @@ ranged as (
         guidance_appointment_choice,
         stronger_nudge_status,
         surfacing_event
+),
 
 is_current as (
     select
